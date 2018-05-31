7fb535087935500a590f2bd1bb784f57a52cfecd
ostapmlnk <ostap.melnik.1996@mail.ru>
2018-05-31 17:12:30 +0300

    fix DB

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_05_29_143529) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "summary"
